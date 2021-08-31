9a697d8cee3988142fe75b6f48f15260ee98e32b
sahaypratik <sahaypratik@gmail.com>
2021-08-31 13:10:16 +0530

    first commit

---
+++ ToasterLibrary/src/main/java/com/winuall/connect/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package com.winuall.connect.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
